04ac8092b3aae9b7489105c3f981115a07ddf3a6
thestackshepherd <jadenmschwartz00@gmail.com>
2026-08-24 23:44:01 -0500

    Refactor Planets model: update field types and remove unused imports

--- src/sbapi/app/models/sbapi_sqlalchemy_model_planets.py
+++ src/sbapi/app/models/sbapi_sqlalchemy_model_planets.py
@@ -1,11 +1,12 @@
-from datetime import datetime
-
-from sqlalchemy import DateTime, Integer, String
-from sqlalchemy.dialects.postgresql import ARRAY
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, String, Integer
+from typing import List, Optional
 
 
 class Base(DeclarativeBase):
+    """_summary_
+    
+    """
     pass
 
 
@@ -13,12 +14,9 @@ class Planets(Base):
     __tablename__ = "planets"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String, primary_key=False)
-    ruler: Mapped[str] = mapped_column(String, nullable=True)
-    affiliation: Mapped[str] = mapped_column(String, nullable=True)
-    residents: Mapped[list[str]] = mapped_column(ARRAY(String), nullable=True)
-    films: Mapped[list[str]] = mapped_column(ARRAY(String), nullable=True)
-    parody_of: Mapped[str] = mapped_column(String, nullable=True)
-    created: Mapped[datetime] = mapped_column(DateTime)
-    edited: Mapped[datetime] = mapped_column(DateTime)
-    url: Mapped[str] = mapped_column(String, unique=True)
+    name: Mapped[String] = mapped_column(String, primary_key=False)
+    ruler: Mapped[String] = mapped_column(String, primary_key=False)
+    affiliation: Mapped[String] = mapped_column(String, primary_key=False)
+    residents: Mapped[String] = mapped_column(String, )
+    films: Mapped[String] = mapped_column(String)
+    parody_of: Mapped[String] = mapped_column(String, ForeignKey=True)
